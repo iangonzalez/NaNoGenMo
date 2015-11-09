01e08e903740cf1327153df4bfb79748fe8994e9
Ian Gonzalez <gonzalez.isv@gmail.com>
2015-11-08 23:26:30 -0500

    added comments, clarity

--- NaNoGenMo/NaNoGenMo/NaNoGenMo.py
+++ NaNoGenMo/NaNoGenMo/NaNoGenMo.py
@@ -5,22 +5,29 @@ import re
 import nltk.data
 import random
 
-bs_grammar = randsent.read_grammar("republic_rebuffs_grammar.txt")
+
 
 corpus_file = "data/the_republic.txt"
 
-print("reading corpus.")
+print("reading The Republic corpus.")
 corpus_string = ""
 with open(corpus_file) as corpus:
     for line in corpus:
         if re.search(r"\S", line):
             corpus_string += line
 
+# use nlkt to tokenize the corpus into sentences
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 sentences = sent_detector.tokenize(re.sub(r"\n", " ", corpus_string))
 
+
+print("adding fun edits to corpus.")
+
+# read in the CFG for generating the confrontational replies to socrates.
+bs_grammar = randsent.read_grammar("republic_rebuffs_grammar.txt")
+
 for i,sent in enumerate(sentences):
-    #replace fawning replies
+    #replace fawning replies with profane rebuffs.
     if len(sent.split(" ")) < 4 and re.search(r"yes|certain|true|just|so|obvious|socrates|sure|exact|clear", sent.lower()):
         sentences[i] = (randsent.randsentence("ROOT", bs_grammar))
 
@@ -30,6 +37,6 @@ for i,sent in enumerate(sentences):
         sent[random.randint(0, len(sent)-2)] +=  ", like,"
         sentences[i] = " ".join(sent)
         
-
+# output the sentences to output1.txt
 with open("outputs/output1.txt", "w") as outf:
     outf.write("\n".join(sentences))
