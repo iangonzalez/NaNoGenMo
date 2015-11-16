6c680afde2b23ab2280a384f4f5c647d4d6a6ba7
Ian Gonzalez <gonzalez.isv@gmail.com>
2015-11-16 02:33:28 -0500

    reorganized and encapsulated existing code

--- NaNoGenMo/NaNoGenMo/NaNoGenMo.py
+++ NaNoGenMo/NaNoGenMo/NaNoGenMo.py
@@ -6,37 +6,59 @@ import nltk.data
 import random
 
 
+class LanguageGenerator:
+    def __init__(self):
+        self.corpus_file = None
+        self.corpus_string = None
+        self.gen_sentences = None
 
-corpus_file = "data/the_republic.txt"
+    def readCorpusFile(self, corpus_file):
+        # read the corpus of text from a file
+        with open(corpus_file) as corpus:
+            corpus_strings = [line for line in corpus if re.search(r"\S", line)]
+        self.corpus_string = "".join(corpus_strings)
 
-print("reading The Republic corpus.")
-corpus_string = ""
-with open(corpus_file) as corpus:
-    for line in corpus:
-        if re.search(r"\S", line):
-            corpus_string += line
+    def sentenceTokenizeCorpus(self):
+        # tokenize the corpus into sentences using the ntlk.punkt module
+        if self.corpus_string is None:
+            return None
 
-# use nlkt to tokenize the corpus into sentences
-sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-sentences = sent_detector.tokenize(re.sub(r"\n", " ", corpus_string))
+        sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+        return sent_detector.tokenize(re.sub(r"\n", " ", self.corpus_string))
+
+    def crassifySocraticDialogue(self):
+        sentences = self.sentenceTokenizeCorpus()
+        if sentences is None:
+            return None
+
+        # read in the CFG for generating the confrontational replies to socrates.
+        bs_grammar = randsent.read_grammar("republic_rebuffs_grammar.txt")
+
+        for i,sent in enumerate(sentences):
+            #replace fawning replies with profane rebuffs.
+            if len(sent.split(" ")) < 4 and re.search(r"yes|certain|true|just|so|obvious|socrates|sure|exact|clear", sent.lower()):
+                sentences[i] = (randsent.randsentence("ROOT", bs_grammar))
+
+            #intersperse likes into questions
+            if sent[-1] == "?" and len(sent.split(" ")) > 3: # sentence is a question
+                sent = sent.split(" ")
+                sent[random.randint(0, len(sent)-2)] +=  ", like,"
+                sentences[i] = " ".join(sent)
+
+        self.gen_sentences = sentences
+        return sentences
+
+    def writeOutput(self, outfile="outputs/output1.txt"):
+        # output the sentences to output1.txt
+        with open(outfile, "w") as outf:
+            outf.write("\n".join(self.gen_sentences))
 
 
-print("adding fun edits to corpus.")
+if __name__ == '__main__':
+    langGenerator = LanguageGenerator()
+    corpus_file = "data/the_republic.txt"
 
-# read in the CFG for generating the confrontational replies to socrates.
-bs_grammar = randsent.read_grammar("republic_rebuffs_grammar.txt")
-
-for i,sent in enumerate(sentences):
-    #replace fawning replies with profane rebuffs.
-    if len(sent.split(" ")) < 4 and re.search(r"yes|certain|true|just|so|obvious|socrates|sure|exact|clear", sent.lower()):
-        sentences[i] = (randsent.randsentence("ROOT", bs_grammar))
-
-    #intersperse likes into questions
-    if sent[-1] == "?" and len(sent.split(" ")) > 3: # sentence is a question
-        sent = sent.split(" ")
-        sent[random.randint(0, len(sent)-2)] +=  ", like,"
-        sentences[i] = " ".join(sent)
-        
-# output the sentences to output1.txt
-with open("outputs/output1.txt", "w") as outf:
-    outf.write("\n".join(sentences))
+    langGenerator.readCorpusFile(corpus_file)
+    langGenerator.crassifySocraticDialogue()
+    langGenerator.writeOutput()
+    
