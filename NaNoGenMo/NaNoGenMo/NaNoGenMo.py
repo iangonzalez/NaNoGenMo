d4a006d3a55e3223002deecc3c04f9bd607ef67d
Ian Gonzalez <gonzalez.isv@gmail.com>
2015-11-22 15:14:10 -0500

    added longer sample markov output. added sentence snip n shuffle algorithm to cut up sentences and paste them back together on shared words.

--- NaNoGenMo/NaNoGenMo/NaNoGenMo.py
+++ NaNoGenMo/NaNoGenMo/NaNoGenMo.py
@@ -2,6 +2,7 @@
 
 import randsent
 import re
+import math
 import nltk.data
 import random
 from collections import defaultdict
@@ -15,6 +16,7 @@ class LanguageGenerator:
         self.gen_sentences = None
         self.markov_model = None
         self.ngram_degree = None
+        self.corpus_sentences = None
 
     def readCorpusFile(self, corpus_file):
         # read the corpus of text from a file
@@ -28,12 +30,15 @@ class LanguageGenerator:
             return None
 
         sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-        return sent_detector.tokenize(re.sub(r"\n", " ", self.corpus_string))
+        self.corpus_sentences = sent_detector.tokenize(re.sub(r"\n", " ", self.corpus_string))
+        return self.corpus_sentences
 
     def crassifySocraticDialogue(self):
-        sentences = self.sentenceTokenizeCorpus()
+        # needs to generate a copy of sentence list (so as not to modify it unnecessarily)
+        sentences = self.corpus_sentences
         if sentences is None:
-            return None
+            sentences = sentenceTokenizeCorpus()
+        sentences = sentences[:]
 
         # read in the CFG for generating the confrontational replies to socrates.
         bs_grammar = randsent.read_grammar("republic_rebuffs_grammar.txt")
@@ -63,9 +68,10 @@ class LanguageGenerator:
         self.ngram_degree = n
       
         self.markov_model = defaultdict(lambda : defaultdict(int))
-        sentences = self.sentenceTokenizeCorpus()
+
+        sentences = self.corpus_sentences
         if sentences is None:
-            return None
+            sentences = self.sentenceTokenizeCorpus()
 
         print("Training markov model on corpus.")
 
@@ -98,7 +104,7 @@ class LanguageGenerator:
             cur_token_list = ["#"] * self.ngram_degree
             sentence = []
             while True:
-                print(sentence, len(sentence))
+                #print(sentence, len(sentence))
                 cur_token = " ".join(cur_token_list)
                 sum_freqs = sum([self.markov_model[cur_token][key] for key in self.markov_model[cur_token]])
                 rand_choice = random.randint(0, sum_freqs)
@@ -122,7 +128,53 @@ class LanguageGenerator:
 
         return self.gen_sentences
 
+    # cuts a random sentence, then finds a matching sentence to use as an ending to that sentence
+    def sentenceSnipNShuffle(self, num_sentences = 1):
+        sentences = self.corpus_sentences
+        if sentences is None:
+            sentences = self.sentenceTokenizeCorpus()
+        sentences = sentences[:]
 
+        word_tokenizer = RegexpTokenizer(r"\w+")
+
+        self.gen_sentences = []
+
+        for i in range(num_sentences):
+            start_sent_str = random.choice(sentences)
+            start_sent = word_tokenizer.tokenize(start_sent_str)
+
+            # choose place to cut initial sentence based on normal distribution centered on the middle of sentence.
+            mu = float(len(start_sent))/2.0                 # center = average
+            sigma = math.sqrt(float(len(start_sent))/8.0)   # stdev = 1/6 sentence length 
+            cut_loc = random.normalvariate(mu, sigma)
+            while cut_loc < 0 or cut_loc >= len(start_sent):
+                cut_loc = random.normalvariate(mu, sigma)
+
+            first_half = start_sent[:int(math.floor(cut_loc))]
+            if not first_half:
+                continue  # nothing to do if first half is empty
+
+            joining_word = first_half[-1]
+            print(joining_word)
+
+            # find all candidate sentences that have the joining word (last word of first half)
+            candidate_sents = [sent for sent in sentences if (sent.find(" " + joining_word + " ") != -1 and sent != start_sent_str)]
+            if not candidate_sents:
+                continue
+            
+            # matches the lowest index matching word in the chosen candidate sentence
+            second_sent = word_tokenizer.tokenize(random.choice(candidate_sents))
+            try:
+                second_half = second_sent[second_sent.index(joining_word)+1:]
+            except:
+                continue
+
+            self.gen_sentences.append(" ".join(first_half + second_half))
+
+        return self.gen_sentences
+            
+
+# Unit tests for the language generator class
 class GeneratorUnitTests:
     def crassifyTest(self):
         langGenerator = LanguageGenerator()
@@ -134,13 +186,20 @@ class GeneratorUnitTests:
 
     def markovLanguageTest(self):
         langGenerator = LanguageGenerator()
-        corpus_file = "data/the_republic.txt"
-        langGenerator.readCorpusFile(corpus_file)
+        langGenerator.readCorpusFile("data/the_republic.txt")
         langGenerator.trainMarkovChain(n=2)
-        print(langGenerator.genMarkovSentences(num_sentences=1))
+        langGenerator.genMarkovSentences(num_sentences = 100)
+        langGenerator.gen_sentences = [sent.split(" #")[0] + "." for sent in langGenerator.gen_sentences]
+        langGenerator.writeOutput("outputs/republic_markov_output.txt")
 
+    def snipSentenceTest(self):
+        langGenerator = LanguageGenerator()
+        langGenerator.readCorpusFile("data/the_republic.txt")
+        print(langGenerator.sentenceSnipNShuffle())
 
 if __name__ == '__main__':
     tester = GeneratorUnitTests()
     #tester.crassifyTest()
-    tester.markovLanguageTest()
+    #tester.markovLanguageTest()
+    tester.snipSentenceTest()
+    
